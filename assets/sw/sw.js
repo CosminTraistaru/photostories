6bb21b692006173f6950164a63bd9d123f5d078f
Cosmin Traistaru <cosmin@traista.ru>
2017-01-24 19:02:56 +0200

    service worker

---
+++ assets/sw/sw.js
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+importScripts('/assets/sw/sw-toolbox.js');
+
+const config = {
+  offlinePage: '/404.html'
+};
+
+config.filesToCache = [
+    '/',
+    config.offlinePage,
+    '/assets/favicon/favicon-16x16.png',
+    '/assets/sw/sw.js',
+    '/about/',
+    '/assets/favicon/manifest.json'
+];
+
+/**
+ * Generates a placeholder SVG image of the given size.
+ */
+function offlineImage(name, width, height) {
+  return `<?xml version="1.0"?>
+<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <g fill="none" fill-rule="evenodd"><path fill="#F8BBD0" d="M0 0h${width}v${height}H0z"/></g>
+  <text text-anchor="middle" x="${Math.floor(width / 2)}" y="${Math.floor(height / 2)}">image offline (${name})</text>
+<style><![CDATA[
+text{
+  font: 48px Roboto,Verdana, Helvetica, Arial, sans-serif;
+}
+]]></style>
+</svg>`;
+}
+/**
+ * Returns true if the Accept header contains the given content type string.
+ */
+function requestAccepts(request, contentType) {
+  return request.headers.get('Accept').indexOf(contentType) != -1;
+}
+
+/**
+ * ampbyexample.com fetch handler:
+ *
+ * - one-behind caching
+ * - shows offline page
+ * - generates placeholder image for unavailable images
+ */
+function photoHandler(request, values) {
+  // for samples show offline page if offline and samples are not cached
+  if (requestAccepts(request, 'text/html')) {
+    // never use cached version for AMP CORS requests (e.g. amp-live-list)
+    if (request.url.indexOf("__amp_source_origin") != -1) {
+      return toolbox.networkOnly(request, values);
+    }
+    // cache or network - whatever is fastest
+    return toolbox.fastest(request, values).catch(function() {
+        return toolbox.cacheOnly(new Request(config.offlinePage), values);
+    });
+  }
+  // always try to load images from the cache first
+  // fallback to placeholder SVG image if offline and image not available
+  if (requestAccepts(request, 'image/')) {
+    return toolbox.cacheFirst(request, values).catch(function() {
+      const url = request.url;
+      const fileName = url.substring(url.lastIndexOf('/') + 1);
+      // TODO use correct image dimensions
+      return new Response(offlineImage(fileName, 1080, 610),
+          { headers: { 'Content-Type': 'image/svg+xml' } }
+      );
+    });
+  } else {
+    // cache all other requests
+    return toolbox.fastest(request, values);
+  }
+}
+
+toolbox.options.debug = false;
+toolbox.router.default = toolbox.networkOnly;
+toolbox.router.get('/(.*)', photoHandler, {origin: self.location.origin});
+toolbox.precache(config.filesToCache);
