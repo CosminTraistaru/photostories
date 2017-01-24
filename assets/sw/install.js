fd475178d2c4113ac0983b6be862e71c9ffe46fb
Cosmin Traistaru <cosmin@traista.ru>
2017-01-24 19:07:37 +0200

    sw

--- assets/sw/install.js
+++ assets/sw/install.js
@@ -1,11 +1,9 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/assets/sw/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    .then(function(registration) {
+          console.log('Service Worker Registered');
     });
+  navigator.serviceWorker.ready.then(function(registration) {
+     console.log('Service Worker Ready');
   });
 }
